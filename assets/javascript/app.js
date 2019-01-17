964a87097a8a3d06a06abbb32932e779e224a306
LAMorris <morris.leaha@gmail.com>
2019-01-17 01:17:26 -0600

    took it back a step and pseudocoded from beginning to end, placing what I had already done in each step

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,8 +1,49 @@
-// initial array of fashion designers 
-var designers = ["Alexander McQueen", "Chanel", "Dior", "Marc Jacobs", "Prada", "Louis Vuitton", "Gucci"];
+// DISPLAY STARTING BUTTONS 
+// create a function to display starting fashion designer buttons
+    // create an initial array (store as var topics = ...)
+    var topics = ["Alexander McQueen", "Chanel", "Dior", "Marc Jacobs", "Prada", "Louis Vuitton", "Gucci"];
+    // create a for loop to go through all the values of the array
+        // create a button for each array value
+        // display buttons to page 
 
-// store API URL and my API key to search within GIPHY
-var queryURL = "https://api.giphy.com/v1/gifs/search?" + designers + "&api_key=0C11n5NgNzl0OC5zLvtsDkQ8Vv2IHwGI&limit=10";
+// WHEN BUTTON IS CLICKED, RUN FXN TO GET INFORMATION FROM API & DISPLAY IT
+// when a fashion designer button is clicked, create a function that GETS information from GIPHY API:
+    // make AJAX call to GIPHY API
+    var queryURL = "https://api.giphy.com/v1/gifs/search?" + designers + "&api_key=0C11n5NgNzl0OC5zLvtsDkQ8Vv2IHwGI&limit=10";
+
+    $.ajax({
+        url: queryURL,
+        method: "GET",
+    }).then(function(response) {
+       // console.log the "response" as an object
+        console.log(response);
+        // store the response.STATIC path in a variable
+            // display the 10 random STATIC gifs to page
+        // store the response.RATING path in a variable
+            // display the rating to the page (under the gif)
+        // store the response.ANIMATED path in a variable
+    });
+
+// FXNS FOR WHEN THE GIF IS CLICKED:
+    // when the gif is clicked (odd number of times):
+        // create a function that:
+            // displays the animated gif to the page
+
+    // when the gif is clicked again (even number of times):
+        // create a function that:
+            // displays the static gif
+            
+            
+
+ // ==================================OPTIONAL/EXTRA=====================================================================       
+            
+// USE VALUE FROM USER TEXT TO CREATE NEW BUTTON
+// when a user types a value in text-input field:
+    // when submit button is clicked, then:
+        // create a function that grabs the value from text input and:
+            // pushes that value into the array
+                // call function that makes the button & displays on in HTML
+
 
 // Create the AJAX call
 $.ajax({
@@ -11,16 +52,4 @@ $.ajax({
 }).then(function (response) {
     console.log(response);
 });
-
-
-// create a function to make buttons with initial array of fashion designers
-    // empty buttons prior to user adding new buttons
-    // loop through designers array to create initial buttons
-    // display initial array of fashion designer buttons when page loads
-    
-// create an event listener function when user clicks "add" button 
-    // utilize .preventDefault() method on event 
-    // grab value from text input
-    // push value into array
-    // call function to create button
     
